3d1f059d3ec4d086e157fd63ca52de335b568534
Tom Whale <tom.whale@trinitymirror.com>
2018-03-23 11:54:04 +0000

    Rename Routes

--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import PartnerContainer from './containers/PartnerContainer'
 
-
 const Routes = () => (
     <Router>
       <Switch>
